436e78f93b49f945fac21a8d360ce510f0acd8c9
DylanB5402 <d.barva5402@gmail.com>
2024-08-10 15:05:22 -0700

    Update ws logic

--- app/pages/api/url.ts
+++ app/pages/api/url.ts
@@ -14,6 +14,7 @@ export default async function handler (req: NextApiRequest, res: NextApiResponse
   if (req.method === 'POST') {
     const url = req.body.url
     try {
+      urlSenderWs.on("open", () => {});
       urlSenderWs.send(url)
       console.log(687)
       const story = await prisma.story.create({
@@ -23,8 +24,9 @@ export default async function handler (req: NextApiRequest, res: NextApiResponse
       })
       console.log(story)
       res.status(200).json({ urlAccepted: true, url })
-    } catch {
-      res.status(200).json({ urlAccepted: false, url: undefined })
+    } catch (error : {code : string} | unknown) {
+      urlSenderWs.send(url)
+      res.status(200).json({ urlAccepted: true, url: undefined })
     }
   } else {
     res.status(200).json({ urlAccepted: false, url: undefined })
